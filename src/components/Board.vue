c08bc929fd1427cc7685b433c4ed687e8e85fb8f
vhxubo <vhxubo@gmail.com>
2021-04-02 12:17:53 +0800

    update no description show name

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -12,7 +12,7 @@
         <div
           class="shadow rounded-md px-4 md:px-6 py-2 md:py-3 m-1.5 md:m-2 cursor-pointer select-none hover:shadow-md dark:bg-gray-700 text-sm md:text-base"
           @click="openUrl(item.url)"
-          :title="item?.description"
+          :title="item.description ? item.description : item.name"
         >
           {{ item.name }}
         </div>
